caa55316f11d9270c6c547f986f15d9219b54965
868system <96748378+868system@users.noreply.github.com>
2022-12-11 17:47:57 -0600

    silent wget output

--- run.sh
+++ run.sh
@@ -6,12 +6,9 @@
 # Data Source: CoinMarketCap via Yahoo! Finance
 # https://finance.yahoo.com/quote/ETH-USD/history
 
-echo
-
 wget 'https://query1.finance.yahoo.com/v7/finance/download/ETH-USD?period1=0&period2=10000000000&interval=1d&events=history&includeAdjustedClose=true' \
     --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+    --quiet \
     --output-document='data/eth_usd.csv'
-
-echo
 
 node ./app.js $1
