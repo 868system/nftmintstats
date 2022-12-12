e0b9f625ced7f00fde7cd80e8f9de781cf963ae5
868system <96748378+868system@users.noreply.github.com>
2022-12-12 14:32:46 -0600

    move the console output section to a separate file

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 import { get } from 'https';
 import { argv } from 'process';
-import { projects } from './projects.js';
 import { writeFile, readFileSync } from 'fs';
+
+import { projects } from './projects.js';
+import { printCharts } from './visualizers.js'
+
 
 const myArgs = argv.slice(2);
 
@@ -13,8 +16,6 @@ const showError = (err) => {
 
 const projectName = myArgs[0]
 const project = projects(projectName);
-
-
 
 
 // Preload Ethereum price data
@@ -47,7 +48,9 @@ const getEthPrice = (date) => {
 };
 
 
-const process = (allTransactions) => {
+const process = (dataSets) => {
+
+    const allTransactions = dataSets['transactions'];
 
     // Rough pruning: remove ERC721 transfers and approvals
     const mostTransactions = allTransactions.filter(x => !(['setapprovalforall', 'safetransferfrom', 'transferfrom'].includes(x['functionName'].split('(')[0].toLowerCase())));
@@ -107,57 +110,7 @@ const process = (allTransactions) => {
         return result;
     });
 
-    const itemsTotal = mintTransactions.reduce((acc, tx) => parseInt(acc) + parseInt(tx['_numberMinted']), 0);
-    const items = Object.keys(project.mintFunctions).map( methodId =>
-        mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseInt(acc) + parseInt(tx['_numberMinted']) : parseInt(acc), 0) );
-
-    const costsETH = Object.keys(project.mintFunctions).map( methodId =>
-        Math.round(mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseFloat(acc) + parseFloat(tx['_valueETH']) : parseFloat(acc), 0.0) * 100.0) / 100.0);
-    const costsUSD = Object.keys(project.mintFunctions).map( methodId =>
-        Math.round(mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseFloat(acc) + parseFloat(tx['_valueUSD']) : parseFloat(acc), 0.0) * 100.0) / 100.0);
-
-    const costTotalETH  =  Math.round(mintTransactions.reduce((acc, tx) => parseFloat(acc) + parseFloat(tx['_valueETH']), 0.0) * 100.0) / 100.0;
-    const costTotalUSD  =  Math.round(mintTransactions.reduce((acc, tx) => parseFloat(acc) + parseFloat(tx['_valueUSD']), 0.0) * 100.0) / 100.0;
-
-
-    // TODO: Maybe this console output section can be relocated
-
-    // Column widths
-    const widthLabel = (Object.values(project.mintFunctions).reduce((a,x) => x[1].length > a ? x[1].length : a, 0) + 1).toLocaleString(undefined);
-    const width1 = costTotalETH.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).length + 1;
-    const width2 = costTotalUSD.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).length + 1;
-
-    console.log('');
-    console.log('# Mint count');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'Items'.padStart(width1) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + items[idx].toLocaleString(undefined).padStart(width1) + ' |'));
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    console.log('| ' + 'TOTAL'.padEnd(widthLabel) + '|' + itemsTotal.toLocaleString(undefined).padStart(width1) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-
-    console.log('');
-    console.log('# Mint value');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
-    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'ETH'.padStart(width1) + ' |' + 'USD'.padStart(width2) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
-    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + costsETH[idx].toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |' + costsUSD[idx].toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width2) + ' |'));
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
-    console.log('| ' + 'TOTAL'.padEnd(widthLabel) + '|' + costTotalETH.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |' + costTotalUSD.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width2) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
-
-    console.log('');
-    console.log('# Computed value per item');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'ETH'.padStart(width1) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + (Math.round((costsETH[idx]/items[idx]) * 100.0) / 100).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |'));
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-    console.log('| ' + 'AVERAGE'.padEnd(widthLabel) + '|' + (costTotalETH/itemsTotal).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |');
-    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
-
-    console.log('');
+    printCharts(project, mintTransactions);
 
     writeFile('data/' + projectName + '_raw.json', JSON.stringify(mostTransactions), showError);
     writeFile('data/' + projectName + '_parsed.json', JSON.stringify(mintTransactions), showError);
@@ -185,7 +138,7 @@ const download = async (urls, _dataSets, _currentDataSetKey, _currentContent) =>
     // the same keys as urls, and dataSetKey will be undefined
     if (dataSetKey === undefined) {
         // Proceed to process the datasets
-        process(dataSets['transactions']);
+        process(dataSets);
     }
     else {
         const url = urls[dataSetKey];
---
+++ visualizers.js
@@ -0,0 +1,57 @@
+
+const printCharts = (project, mintTransactions) => {
+
+    const itemsTotal = mintTransactions.reduce((acc, tx) => parseInt(acc) + parseInt(tx['_numberMinted']), 0);
+    const items = Object.keys(project.mintFunctions).map( methodId =>
+        mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseInt(acc) + parseInt(tx['_numberMinted']) : parseInt(acc), 0) );
+
+    const costsETH = Object.keys(project.mintFunctions).map( methodId =>
+        Math.round(mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseFloat(acc) + parseFloat(tx['_valueETH']) : parseFloat(acc), 0.0) * 100.0) / 100.0);
+    const costsUSD = Object.keys(project.mintFunctions).map( methodId =>
+        Math.round(mintTransactions.reduce((acc, tx) => tx['methodId'] == methodId ? parseFloat(acc) + parseFloat(tx['_valueUSD']) : parseFloat(acc), 0.0) * 100.0) / 100.0);
+
+    const costTotalETH  =  Math.round(mintTransactions.reduce((acc, tx) => parseFloat(acc) + parseFloat(tx['_valueETH']), 0.0) * 100.0) / 100.0;
+    const costTotalUSD  =  Math.round(mintTransactions.reduce((acc, tx) => parseFloat(acc) + parseFloat(tx['_valueUSD']), 0.0) * 100.0) / 100.0;
+
+    // Column widths
+    const widthLabel = (Object.values(project.mintFunctions).reduce((a,x) => x[1].length > a ? x[1].length : a, 0) + 1).toLocaleString(undefined);
+    const width1 = costTotalETH.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).length + 1;
+    const width2 = costTotalUSD.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).length + 1;
+
+    console.log('');
+    console.log('# Mint count');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'Items'.padStart(width1) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + items[idx].toLocaleString(undefined).padStart(width1) + ' |'));
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    console.log('| ' + 'TOTAL'.padEnd(widthLabel) + '|' + itemsTotal.toLocaleString(undefined).padStart(width1) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+
+    console.log('');
+    console.log('# Mint value');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
+    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'ETH'.padStart(width1) + ' |' + 'USD'.padStart(width2) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
+    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + costsETH[idx].toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |' + costsUSD[idx].toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width2) + ' |'));
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
+    console.log('| ' + 'TOTAL'.padEnd(widthLabel) + '|' + costTotalETH.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |' + costTotalUSD.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width2) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padEnd(1 + width1, '-') + '+' + '-'.padEnd(1 + width2, '-') + '+');
+
+    console.log('');
+    console.log('# Computed value per item');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    console.log('| ' + 'Category'.padEnd(widthLabel) + '|' + 'ETH'.padStart(width1) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    Object.values(project.mintFunctions).forEach((item, idx) => console.log('| ' + item[1].padEnd(widthLabel) + '|' + (Math.round((costsETH[idx]/items[idx]) * 100.0) / 100).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |'));
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+    console.log('| ' + 'AVERAGE'.padEnd(widthLabel) + '|' + (costTotalETH/itemsTotal).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}).padStart(width1) + ' |');
+    console.log('+-' + '-'.padEnd(widthLabel,'-') + '+' + '-'.padStart(width1, '-') + '-+');
+
+    console.log('');
+}
+
+
+
+
+export { printCharts };
