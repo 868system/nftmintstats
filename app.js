ea0ca5394475bf36b8c1a4a0fd9722aefadc0469
868system <96748378+868system@users.noreply.github.com>
2022-12-13 12:07:33 -0600

    Dev snapshot, currently working on txs that use Gnosis Safe and similar things

--- app.js
+++ app.js
@@ -48,9 +48,80 @@ const getEthPrice = (date) => {
 };
 
 
-const process = (dataSets) => {
+const process = (allTransfers, allTransactions) => {
 
-    const allTransactions = dataSets['transactions'];
+    // One transfer per token only
+    // (overlap is expected because of how we download from Etherscan)
+    const uniqueTokenTransfers = allTransfers.filter((x, i, a) => a.findIndex((y) => y['tokenID'] == x['tokenID']) == i);
+
+    // Construct an object containing each token's complete mint information
+    // Use data from both transfers and transactions
+    const tokenMints = uniqueTokenTransfers.map(thisTokenTransfer => {
+
+        const thisTokenTransaction = allTransactions.find(x => x['isError'] == 0 && x['hash'] == thisTokenTransfer['hash']);
+
+        if (thisTokenTransaction === undefined) {
+            console.log('Warning: The contract used in transaction: \n' + thisTokenTransfer['hash'] + '\nneeds to be added to projects.js\n');
+            //writeFile('data/' + projectName + '_problem_transfer.json', JSON.stringify(thisTokenTransfer), showError);
+        }
+
+        const thisTokenMint = {
+            // Common fields
+            'blockNumber'       : thisTokenTransfer['blockNumber'],
+            'timeStamp'         : thisTokenTransfer['timeStamp'],
+            'hash'              : thisTokenTransfer['hash'],
+            'nonce'             : thisTokenTransfer['nonce'],
+            'blockHash'         : thisTokenTransfer['blockHash'],
+            'transactionIndex'  : thisTokenTransfer['transactionIndex'],
+            'gas'               : thisTokenTransfer['gas'],
+            'gasPrice'          : thisTokenTransfer['gasPrice'],
+            'gasUsed'           : thisTokenTransfer['gasUsed'],
+            'cumulativeGasUsed' : thisTokenTransfer['cumulativeGasUsed'],
+
+            // Transfer
+            'tokenID'      : thisTokenTransfer['tokenID'],
+            'tokenName'    : thisTokenTransfer['tokenName'],
+            'tokenSymbol'  : thisTokenTransfer['tokenSymbol'],
+            'tokenDecimal' : thisTokenTransfer['tokenDecimal'],
+
+            // Transaction
+            'value'            : thisTokenTransaction['value'],
+            'isError'          : thisTokenTransaction['isError'],
+            'txreceipt_status' : thisTokenTransaction['txreceipt_status'],
+            'methodId'         : thisTokenTransaction['methodId'],
+            'functionName'     : thisTokenTransaction['functionName'],
+
+            // Collisions Transfer
+            // "from": "0x0000000000000000000000000000000000000000",
+            // "to": "0xc4f4325490842426816764958a234857df4d150a",
+            'contractAddress' : thisTokenTransfer['contractAddress'],
+            // "input": "deprecated",
+
+            // Collisions Transaction
+            // "from": "0xc4f4325490842426816764958a234857df4d150a",
+            // "to": "0x160c404b2b49cbc3240055ceaee026df1e8497a0",
+            // "contractAddress": "",
+            'input' : thisTokenTransaction['input'],
+
+
+            // Discarded
+            // "confirmations": "1456461"
+        };
+        return thisTokenMint;
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 
     // Rough pruning: remove ERC721 transfers and approvals
     const mostTransactions = allTransactions.filter(x => !(['setapprovalforall', 'safetransferfrom', 'transferfrom'].includes(x['functionName'].split('(')[0].toLowerCase())));
@@ -64,7 +135,6 @@ const process = (dataSets) => {
     // Remove entries that resulted in errors
     const mintTransactionsSuccessful = mintTransactionsUnique.filter(x => x['isError'] == 0);
 
-    // Discard info we won't use, especially non-static fields like 'confirmations'
     const mintTransactions = mintTransactionsSuccessful.map(x => {
 
         const isoDate = new Date(parseInt(x['timeStamp']) * 1000);
@@ -112,36 +182,36 @@ const process = (dataSets) => {
 
     printCharts(project, mintTransactions);
 
+    writeFile('data/' + projectName + '_mints.json', JSON.stringify(tokenMints), showError);
+    writeFile('data/' + projectName + '_transfers.json', JSON.stringify(uniqueTokenTransfers), showError);
     writeFile('data/' + projectName + '_raw.json', JSON.stringify(mostTransactions), showError);
     writeFile('data/' + projectName + '_parsed.json', JSON.stringify(mintTransactions), showError);
 }
 
-const urls = {
-    'transfers'    : 'https://api.etherscan.io/api?module=account&action=tokennfttx&address=0x0000000000000000000000000000000000000000&contractaddress=' + project.contractAddress + '&startblock=',
-    'transactions' : 'https://api.etherscan.io/api?module=account&action=txlist&address=' + project.contractAddress + '&startblock='
-}
+const urls = ['https://api.etherscan.io/api?module=account&action=tokennfttx&address=0x0000000000000000000000000000000000000000&contractaddress=' + project.contractAddresses[0] + '&startblock=']
+    .concat(project.contractAddresses.map(x => 'https://api.etherscan.io/api?module=account&action=txlist&address=' + x + '&startblock='))
 
+const download = async (urls, _currentUrlIdx, _transfers, _transactions, _currentContent) => {
 
-const download = async (urls, _dataSets, _currentDataSetKey, _currentContent) => {
+    const currentUrlIdx = _currentUrlIdx !== undefined ? _currentUrlIdx : 0;
+    const transfers     = _transfers     !== undefined ? _transfers     : [];
+    const transactions  = _transactions  !== undefined ? _transactions  : [];
 
-    const currentContent = _currentContent !== undefined ? _currentContent : [];
-
-    const dataSets = _dataSets !== undefined ? _dataSets : {};
-
-    const currentBlock = currentContent.length == 0 ? 0 :
-                         Math.max(...currentContent.map(x => x['blockNumber']));
-
-    const dataSetKey = _currentDataSetKey !== undefined ? _currentDataSetKey :
-                       Object.keys(urls).find(x => !Object.keys(dataSets).includes(x));
-
-    // If all URLs have been retrieved, dataSets will have
-    // the same keys as urls, and dataSetKey will be undefined
-    if (dataSetKey === undefined) {
+    if (currentUrlIdx >= urls.length) {
         // Proceed to process the datasets
-        process(dataSets);
+        process(transfers, transactions);
     }
+
     else {
-        const url = urls[dataSetKey];
+
+        const url = urls[currentUrlIdx];
+
+        // either 'txlist' or 'tokennfttx'
+        const dataSet = url.split('action=')[1].split('&')[0];
+
+        const currentContent = _currentContent !== undefined ? _currentContent : [];
+        const currentBlock = currentContent.length == 0 ? 0 :
+                            Math.max(...currentContent.map(x => x['blockNumber']));
 
         await new Promise(resolve => setTimeout(resolve, 5000));
 
@@ -154,14 +224,16 @@ const download = async (urls, _dataSets, _currentDataSetKey, _currentContent) =>
                     const content = JSON.parse(chunks.join(''))['result'];
                     const updatedContent = currentContent.concat(content);
 
-                    console.log(updatedContent.length + ' records retrieved from \'' + dataSetKey+ '\'' );
+                    console.log(updatedContent.length + ' records retrieved from [' + currentUrlIdx + '] \'' + dataSet + '\'' );
 
                     if (content.length >= 10000) {
-                        download(urls, dataSets, dataSetKey, updatedContent);
+                        download(urls, currentUrlIdx, transfers, transactions, updatedContent);
                     }
                     else {
-                        const updatedDataSet = {...dataSets, [dataSetKey]: updatedContent};
-                        download(urls, updatedDataSet);
+                        const updatedTransfers = dataSet == 'tokennfttx' ? transfers.concat(updatedContent) : transfers;
+                        const updatedTransactions = dataSet == 'txlist' ? transactions.concat(updatedContent) : transactions;
+
+                        download(urls, currentUrlIdx + 1, updatedTransfers, updatedTransactions);
                     }
 
                 });
--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projectDefs = {
 
     'pxn': {
 
-        contractAddress: '0x160c404b2b49cbc3240055ceaee026df1e8497a0',
+        contractAddresses: ['0x160c404b2b49cbc3240055ceaee026df1e8497a0'],
 
         mintFunctions: {
             '0x4bc0a305': ['mintWL'           , 'Whitelist'],
@@ -12,18 +12,18 @@ const projectDefs = {
         },
 
         getNumberMinted: (methodId, input) => {
-            return methodId == '0x4bc0a305' ? 1 :
-                   methodId == '0x59287fab' ||
-                   methodId == '0x7b671780' ? parseInt(input.substring(10, 74), 16) :
-                   methodId == '0x7c69e207' ? 129 :
-                   NaN;
+            return  methodId == '0x4bc0a305' ? 1 :
+                    methodId == '0x59287fab' ||
+                    methodId == '0x7b671780' ? parseInt(input.substring(10, 74), 16) :
+                    methodId == '0x7c69e207' ? 129 :
+                    NaN;
         },
 
     },
 
     'bayc': {
 
-        contractAddress: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+        contractAddresses: ['0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'],
 
         mintFunctions: {
             '0xa723533e': ['mintApe'     , 'Ape Mint'],
@@ -31,26 +31,41 @@ const projectDefs = {
         },
 
         getNumberMinted: (methodId, input) => {
-            return methodId == '0xa723533e' ? parseInt(input.substring(10, 74), 16) :
-                   methodId == '0xb0f67427' ? 30 :
-                   NaN;
+            return  methodId == '0xa723533e' ? parseInt(input.substring(10, 74), 16) :
+                    methodId == '0xb0f67427' ? 30 :
+                    NaN;
         },
     },
 
     // TODO: MAYC not quite working yet... 19343 detected mints out of 19430
     'mayc': {
 
-        contractAddress: '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+        contractAddresses: [
+            '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+            '0xa7ab7a265f274fa664187698932d3cabb851023d',
+            '0x22810e61ddd7df3dcb87dbbffe8f22811806145e',
+            '0xb88f61e6fbda83fbfffabe364112137480398018',
+            '0x0dd46d3cab80d4eec9298fbae9cc6c27edd969c0',
+            '0xdbfd76af2157dc15ee4e57f3f942bb45ba84af24'
+        ],
 
         mintFunctions: {
             '0xc15e24bc': ['mintMutants'        , 'Mint Mutants'],
             '0xe73a9a25': ['mutateApeWithSerum' , 'Mutate Ape With Serum'],
+            '0x6a761202': ['execTransaction'    , 'Gnosis Safe Proxy']
         },
 
         getNumberMinted: (methodId, input) => {
-            return methodId == '0xc15e24bc' ? parseInt(input.substring(10, 74), 16) :
-                   methodId == '0xe73a9a25' ? 1 :
-                   NaN;
+            return  methodId == '0xc15e24bc' ? parseInt(input.substring(10, 74), 16) :
+                    methodId == '0xe73a9a25' ? 1 :
+                    methodId == '0x6a761202' &&
+                        input.substring(34, 74).toLowerCase() == '60e4d786628fea6478f785a6d7e704777c86a7c6' &&
+                        input.substring(714, 722) == 'c15e24bc' ? parseInt(input.substring(722, 786), 16) :
+                    methodId == '0x6a761202' &&
+                        input.substring(34, 74).toLowerCase() == '60e4d786628fea6478f785a6d7e704777c86a7c6' &&
+                        input.substring(714, 722) == 'e73a9a25' ? 1 :
+                    methodId == '0x6a761202' ? 0 :
+                    NaN;
         },
     }
 
